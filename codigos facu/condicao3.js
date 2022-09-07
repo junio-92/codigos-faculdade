f03a5dfdec78ec8bd700cb98b232f4fd5aba526c
Junio Dias <9145junio@gmail.com>
2022-09-07 14:23:42 -0300

    codigos da faculdade

---
+++ codigos facu/condicao3.js
@@ -0,0 +1,6 @@
+var idade=19
+if(idade<18){
+    console.log('menor de idade')
+} else{
+    console.log('maior de idade')
+}
---
+++ codigos facu/condicaose.js
@@ -0,0 +1,22 @@
+
+
+
+
+var estado = 'rio'
+console.log(`Vivendo em ${estado}`)
+if(estado == 'Minas'){
+    console.log('Você é mineiro')
+}else{
+    console.log('Você não é mineiro')
+}
+
+
+
+/////////// velocidade/////////////////
+
+/*var  vel = 9
+console.log (`A velocidade e ${vel} km/h`)
+if (vel >60){
+    console.log(`Você ultrapassou o limite.Multado`)
+}
+console.log(`Continue a dirigir`)*/
