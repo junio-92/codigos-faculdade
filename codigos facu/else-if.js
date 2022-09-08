9d46be8356b58c8c03598f21c580452e40fa96a3
Junio Dias <9145junio@gmail.com>
2022-09-08 15:22:52 -0300

    novo

---
+++ codigos facu/else-if.js
@@ -0,0 +1,29 @@
+var l = 'lingua'
+var p = 'portugues'
+var r= 'russo'
+var e='erro'
+
+if(lingua == 'portugues'){
+		
+    console.log('va para angola'  )
+}
+
+
+
+else if(lingua =='espanhol'){
+    console.log('Va pa o chile')
+}
+
+
+
+else if(lingua == 'russo'){
+
+        console.log('va para a Crimeia')
+        
+}else
+    printf("ERRO")
+
+
+    
+    
+
