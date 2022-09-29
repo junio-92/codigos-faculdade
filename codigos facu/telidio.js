76c751847d172772451e1a158fe9452226f92751
Junio Dias <9145junio@gmail.com>
2022-09-28 23:52:55 -0300

    js

--- codigos facu/telidio.js
+++ codigos facu/telidio.js
@@ -1,1 +1,20 @@
-alert('Olá Telidio!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+
+    var hora = 22
+    msg.innerHTML = ` Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+
+        img.src= ''
+    }else if (hora >= 12 && hora <= 18) {
+
+        img.src=''
+    }else{
+
+        img.src=''
+    }
+    
+}
