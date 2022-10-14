8168c18cd8cfb91489a6669bca40b6ce246b8c7c
Junio Dias <9145junio@gmail.com>
2022-10-13 23:37:36 -0300

    meu banco de dados

--- codigos facu/heroisbh.sql
+++ codigos facu/heroisbh.sql
@@ -1,11 +1,16 @@
-create database herois;
+create database herois
+default character set utf8
+default collate utf8_general_ci;
 use herois;
-create table registroh (
-
-nome varchar (30),
-idade tinyint, 
-poder varchar(30),
-cidade varchar(30)
-);
-
-describe registroh;
+drop database  herois;
+create table heroi(
+id int not null auto_increment,
+nome varchar (30) not null,
+nascimento date,
+cidade varchar(30) default 'Belo Horizonte',
+sexo enum ('M','F'),
+peso decimal(5,2),
+altura decimal (3,2),
+poder char (30),
+primary key (id)
+)default  charset = utf8;
