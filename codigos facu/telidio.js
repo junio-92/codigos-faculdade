66ba153b516c5e3687df6d99423ef837c750d144
Junio Dias <9145junio@gmail.com>
2022-10-27 16:13:57 -0300

    gits

--- codigos facu/telidio.js
+++ codigos facu/telidio.js
@@ -5,26 +5,45 @@ function carregar(){
     const msg = window.document.getElementById('msg')
     let img = window.document.getElementById('imagem')
     const data = new Date()
-    //var hora data = new Date()
-   const hora = 23
+    const hora = data.getHours()
+   //let hora = 13
     msg.innerHTML = ` Agora são ${hora} horas.`
 
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 0 && hora < 12) 
+    {
 
-        img.src='manha.png'
-        body.style.background='	#00008B'
-    }else if (hora >= 12 && hora < 18) {
-
-        img.src='tarde.png'
-        body.style.background='#DAA520'
-    }else{
-
-        img.src='noite.png'
-        body.style.background='#1C1C1C'
-    }
+            img.src='manha.png'
+            body.style.background='	#00008B'
+        }else if (hora >= 12 && hora <= 18) 
+        {
+     
+            img.src='tarde.png'
+            body.style.background='#DAA520'
+         }else{
+     
+            img.src='noite.png'
+            body.style.background='#1C1C1C'
+        }
     
 
-    //////img.src='tarde.png'///////////
+    //////img.src='tarde.png'
+    
+    
+  //  if (hora >= 0 && hora < 12) {
+
+   //    img.src='manha.png'
+   //     body.style.background='	#00008B'
+  //  }else if (hora >= 12 && hora < 18) {
+
+  //      img.src='tarde.png'
+   //     body.style.background='#DAA520'
+  //  }else{
+
+     //   img.src='noite.png'
+    //    body.style.background='#1C1C1C'
+   // }
+    
+    /////////// 
         
 }
 
